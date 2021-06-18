392a64aaa13ff31f42477e8bb43be6801ab4222b
Jean Pierre <frankjeanpierregs@ufps.edu.co>
2021-06-18 07:51:12 -0500

    fix deploy path

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import registermemberstaff from "./pages/RegisterMemberStaff/RegisterMemberStaff
 import listpersonstaff from "./pages/ListPersonStaff/ListPersonStaff";
 import listplayer from "./pages/ListPlayer/ListPlayer";
 import registerfile from "./pages/RegisterFile/RegisterFile";
+import index from "./pages/Index/Index";
 
 import { Route } from "wouter";
 
 export default function App() {
 	return (
 		<div className="App">
+			<Route path="/" component={index} />
 			<Route path="/registrar-cuerpo-tecnico" component={registerstaff} />
 			<Route path="/registrar-ficha-jugador" component={registerfile} />
 			<Route
